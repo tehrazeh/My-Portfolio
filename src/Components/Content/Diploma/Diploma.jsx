c36919a77008ad0bf634541e4f0710cc06f55c52
tehrazeh <danilatolstoukhov@gmail.com>
2022-09-25 15:35:15 -0400

    Refactored and redesigned diploma component

--- src/Components/Content/Diploma/Diploma.jsx
+++ src/Components/Content/Diploma/Diploma.jsx
@@ -3,16 +3,16 @@ import s from './Diploma.module.css'
 const Diploma = (props) => {
     return (
         <div className={s.diplomaContainer}>
-            <h2>
-                Graduated: April, 2022 
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;                  
-                GPA:3.87/4 (Honors)
-            </h2>
+            <div className={s.diplomaBlock}>
+                <div>
+                    <h3>Graduated: April, 2022</h3>
+                </div>
+                <div>
+                    <h3>GPA:3.87/4 (Honors)</h3>
+                </div>
+                
+            </div>
+
             <img className={s.diplomaImage} alt='diploma' src={diplomaImage}/>
         </div>
     )
