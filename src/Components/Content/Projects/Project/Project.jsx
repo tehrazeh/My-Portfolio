21a2f52242aaec14f21b15df53c02e3f3c51be36
tehrazeh <danilatolstoukhov@gmail.com>
2022-11-23 18:01:40 -0500

    Added placeholders with aspect ratios for project previews to prevent CLS and improve UX

--- src/Components/Content/Projects/Project/Project.jsx
+++ src/Components/Content/Projects/Project/Project.jsx
@@ -48,64 +48,66 @@ const Project = (props) => {
 
     return (
         <div className={s.projectContainer}>
-                    <div className={s.projectTitle}>
-                        <p>{props.projectData.title}</p>
-                        <a href={`https://github.com/tehrazeh/${props.projectData.repository}`}>
-                            <button className={s.titleButton} >
-                                <img className={s.titleButtonImage} src={gitHubImage} alt='github' />
-                            </button>
-                        </a>
-                        <Link to={`/projects/${props.projectData.id}/screenshots`}
-                            state={{
-                                images: props.projectData.images,
-                                imagesFolder: `ProjectsImages/${props.projectData.repository}-images`
-                            }}>
-                            <button className={s.titleButton}>
-                                <img className={s.titleButtonImage} src={require(`../ProjectsImages/assets/screenshots.png`)} alt='visibility' />
-                            </button>
-                        </Link>
-                        {props.projectData.deployInfo ? <a href={props.projectData.deployInfo}>{projectLinkButton}</a>
-                            : <Link to={'/notdeployed'}>{projectLinkButton}</Link>}
-                    </div>
-                    <div className={s.skills}>
-                        Skills: {skills}
-                    </div>
-                    <LazyLoadImage className={s.projectPreview} 
-                        src={require(`../ProjectsImages/${props.projectData.repository}-images/preview.jpg`)}
-                        alt='skill pic' loading="lazy" effect="blur"
-                        placeholderSrc={require(`../ProjectsImages/${props.projectData.repository}-images/preview-placeholder.jpg`)}/>
-                    <div className={s.dropdownContainer}>
-                        <div className={s.dropdownElement}>
-                            <button className={s.dropdownButton}>Description</button>
-                            <div className={s.dropdownMenu}>
-                                <div className={s.dropdownMenuContainer}>
-                                    <p>
-                                        {props.projectData.description}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={s.dropdownElement}>
-                            <button className={s.dropdownButton}>Statistics</button>
-                            <div className={s.dropdownMenu}>
-                                <div className={s.dropdownMenuContainer}>
-                                    <p>
-                                        Date Created: {repoStats.dateCreated}
-                                    </p>
-                                </div>
-                                <div className={s.dropdownMenuContainer}>
-                                    <p>
-                                        Total Commits: {repoStats.totalCommits}
-                                    </p>
-                                </div>
-                                <div className={s.dropdownMenuContainer}>
-                                    <p>
-                                        Last Update: {repoStats.lastChange}
-                                    </p>
-                                </div>
-                            </div>
+            <div className={s.projectTitle}>
+                <p>{props.projectData.title}</p>
+                <a href={`https://github.com/tehrazeh/${props.projectData.repository}`}>
+                    <button className={s.titleButton} >
+                        <img className={s.titleButtonImage} src={gitHubImage} alt='github' />
+                    </button>
+                </a>
+                <Link to={`/projects/${props.projectData.id}/screenshots`}
+                    state={{
+                        images: props.projectData.images,
+                        imagesFolder: `ProjectsImages/${props.projectData.repository}-images`
+                    }}>
+                    <button className={s.titleButton}>
+                        <img className={s.titleButtonImage} src={require(`../ProjectsImages/assets/screenshots.png`)} alt='visibility' />
+                    </button>
+                </Link>
+                {props.projectData.deployInfo ? <a href={props.projectData.deployInfo}>{projectLinkButton}</a>
+                    : <Link to={'/notdeployed'}>{projectLinkButton}</Link>}
+            </div>
+            <div className={s.skills}>
+                Skills: {skills}
+            </div>
+            <div className={s.previewPlaceholder}>
+                <LazyLoadImage className={s.projectPreview}
+                    src={require(`../ProjectsImages/${props.projectData.repository}-images/preview.jpg`)}
+                    alt='skill pic' loading="lazy" effect="blur"
+                    placeholderSrc={require(`../ProjectsImages/${props.projectData.repository}-images/preview-placeholder.jpg`)} />
+            </div>
+            <div className={s.dropdownContainer}>
+                <div className={s.dropdownElement}>
+                    <button className={s.dropdownButton}>Description</button>
+                    <div className={s.dropdownMenu}>
+                        <div className={s.dropdownMenuContainer}>
+                            <p>
+                                {props.projectData.description}
+                            </p>
                         </div>
                     </div>
+                </div>
+                <div className={s.dropdownElement}>
+                    <button className={s.dropdownButton}>Statistics</button>
+                    <div className={s.dropdownMenu}>
+                        <div className={s.dropdownMenuContainer}>
+                            <p>
+                                Date Created: {repoStats.dateCreated}
+                            </p>
+                        </div>
+                        <div className={s.dropdownMenuContainer}>
+                            <p>
+                                Total Commits: {repoStats.totalCommits}
+                            </p>
+                        </div>
+                        <div className={s.dropdownMenuContainer}>
+                            <p>
+                                Last Update: {repoStats.lastChange}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
