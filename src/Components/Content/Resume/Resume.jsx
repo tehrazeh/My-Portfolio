d22db2a4b0caa6b4afda267b3025151941cd6a65
tehrazeh <danilatolstoukhov@gmail.com>
2022-12-14 21:57:17 -0500

    Changed content of resume page

--- src/Components/Content/Resume/Resume.jsx
+++ src/Components/Content/Resume/Resume.jsx
@@ -3,36 +3,56 @@ const Resume = (props) => {
     return (
         <div className={s.resume}>
             <div className={s.ResumeBlock}>
-                <h3>Summary</h3>
-                <h4>
-                    Sheridan graduate, Front-End developer. Love to code my ideas into reality.
-                </h4>
+                <div className={s.blockTitle}>
+                    <img src={require('./Images/summary.png')} alt='summary' />
+                    <h3>Summary</h3>
+                </div>
                 <ul>
-                    <li>Enjoy strengthening my skills through practice.</li>
-                    <li>Building apps that solve a problem for people and have nifty features.</li>
-                    <li>Ensuring code is scalable, responsive, and easy to maintain</li>
-                    <li>Polishing skills with practice and never-ending learning.</li>
+                    <li>Started learning JS, HTML and CSS in early <b>2020</b>. Strenghtened and improved knowledge
+                    by building websites for friends and colleagues.</li>
+                    <li>In January <b>2021</b> enrolled in Sheridan College to 
+                    improve fundamentals, learn new technologies and experience team work.</li>
+                    <li>Graduated with
+                    Honors in April <b>2022</b> and been learning new technologies and building my
+                    dream projects ever since.</li>
+                    <li><b>Now</b> looking for a new challenge as a front end developer.</li>              
+                      
                 </ul>
             </div>
             <br></br>
             <div className={s.ResumeBlock}>
-                <h3>Education</h3>
-                <h4>Computer Programming Diploma</h4>
-                <p>Sheridan College, Oakville, ON</p>
-                <p>January 2021 – April 2022</p>
-                <p>Honors, 3.87/4 GPA</p>
+                <div className={s.blockTitle}>
+                    <img src={require('./Images/education.png')} alt='education' />
+                    <h3>Education</h3>
+                </div>
+                <ul>
+                    <li><b>Computer Programming Diploma</b></li>
+                    <li><b>Sheridan College</b>, Oakville, ON</li>
+                    <li>Jan 2021 - Apr 2022. <b>3.87 GPA</b>, Honors</li>
+                    <li><b>Coursework</b>: </li>
+                    <li>
+                        Web Programming, Interactive User Interface Design,
+                        Mobile Web-based Applications
+                        Fundamentals of Software Design, Database Design & Implementation,
+                        Object Oriented Programming 1 & 2 - Java, Web Development,
+                        Data Structures and Algorithm Development - C
+                    </li>
+                </ul>
 
             </div>
             <br></br>
             <div className={s.ResumeBlock}>
-                <h3>Experience</h3>
-                <h4>Associate, Walmart, Dec 2021 - Present</h4>
-                <h4>Freelance, May 2020 - December 2020</h4>
-                <p>The main project was a web application for the sale of concrete rings business.
-                After that, my focus shifted to simple yet eye-catching portfolios
-                for fellow students using JavaScript, HTML, and CSS.
-                Then college started, and I stopped to focus on my studies.
-                </p>
+                <div className={s.blockTitle}>
+                    <img src={require('./Images/experience.png')} alt='experience' />
+                    <h3>Experience</h3>
+                </div>
+                <ul>
+                    <li><b>Freelance Web Developer</b></li>
+                    <li>Mar 2020 - Dec 2020</li>
+                    <li>Built a <b>website</b> for sale of concrete rings business. It had registration and order forms, contacts, and order history</li>
+                    <li>Built 3 simple yet <b>eye-catching portfolios</b> for fellow students using JavaScript, HTML, and CSS.</li>
+                    <li>2 out of 3 students landed a <b>job</b> with the help of portfolio.</li>
+                </ul>
             </div>
         </div>
     )
