e6cde17f080c97dcb6db31e327bf43d39d2bc9fc
tehrazeh <danilatolstoukhov@gmail.com>
2022-12-28 22:32:25 -0500

    Changed not found page

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import error from './assets/error.png'
 import _notFound from './assets/404.png'
 import s from './NotFound.module.css'
 
@@ -7,11 +6,9 @@ const NotFound = () => {
   return (
     <div className={s.wrapper}>
     <div className={s.imageBlock}>
-        <img className={s.notFoundImage} src={error} alt='error'/>
         <img className={s.notFoundImage} src={_notFound} alt='notfound'/>
     </div>
     <div className={s.textBlock}>
-        <h1>Borders are broken</h1>
         <h2>You reached unexplored lands</h2>
     </div>
         
